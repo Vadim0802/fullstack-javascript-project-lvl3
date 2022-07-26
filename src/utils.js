fe1d5057559ccb1480ecb66dc35b4f953e2aad7f
bodia <v.bodyansky@yandex.ru>
2022-07-27 00:30:24 +1000

    remove comments

--- src/utils.js
+++ src/utils.js
@@ -1,11 +1,3 @@
-// https://ru.hexlet.io/courses ---> ru-hexlet-io-courses.html
-// https://ru.hexlet.io/courses ---> ru-hexlet-io-courses_files
-
-// /assets/professions/nodejs.png --->
-// https://ru.hexlet.io/assets/professions/nodejs.png --->
-// ru-hexlet-io-assets-professions-nodejs.png --->
-// ru-hexlet-io-courses_files/ru-hexlet-io-assets-professions-nodejs.png
-
 export const buildFilenameFromUrl = (link, prefix = '') => {
   const urlObj = new URL(link);
   return `${urlObj.hostname}${urlObj.pathname === '/' ? '' : urlObj.pathname}`
